f4d65c6e781be9a7d2d58565d36278f1c7e49bbc
Jael Armas M <jaelarmas.m@gmail.com>
2021-08-22 00:08:00 -0500

    Valid word

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,26 @@ import React, { FC, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { sliceRandomlyArrayInChunks } from './utils';
 import CustomButton from './components/CustomButton';
+import colors from './resources/colors';
 
-const lettersJson = require('./resources/test-board-2.json');
-const clearIcon = require('./resources/clear.png');
+const lettersJson = require('./resources/json/test-board-2.json');
+const dictionaryJson = require('./resources/json/dictionary.json');
+const clearIcon = require('./resources/img/clear.png');
 
 const App: FC = () => {
 
   const [letters] = useState<Array<Array<string>>>(sliceRandomlyArrayInChunks(lettersJson.board, 4));
   const [word, setWord] = useState<string>('');
+  const [validWord, setValidWord] = useState<boolean>(false);
 
   const handleLetterPressed = (letter: string) => {
-    if (word.length < 10) setWord(word + letter);
-    else setWord(letter);
+    let newWord = word;
+    if (word.length < 6) newWord += letter;
+    else newWord = '';
+    setWord(newWord);
+    if (dictionaryJson.words.some((option: string) => option == newWord.toLowerCase()))
+      setValidWord(true);
+    else setValidWord(false);
   }
 
   const renderLetters = () => {
@@ -34,8 +42,8 @@ const App: FC = () => {
       <View>
         <TouchableOpacity style={{ marginBottom: 10 }} disabled={!word} onPress={() => setWord('')}>
           <View style={styles.clearButton}>
-            <Text style={[styles.clearText, { color: word.length > 0 ? '#FF4A4A' : 'grey' }]}>clear word </Text>
-            <Image source={clearIcon} style={[styles.clearIcon, { tintColor: word.length > 0 ? '#FF4A4A' : 'grey' }]} resizeMode={'stretch'} />
+            <Text style={[styles.clearText, { color: word.length > 0 ? colors.RED : colors.GREY }]}>clear word </Text>
+            <Image source={clearIcon} style={[styles.clearIcon, { tintColor: word.length > 0 ? colors.RED : colors.GREY }]} resizeMode={'stretch'} />
           </View>
         </TouchableOpacity>
         <View>
@@ -43,6 +51,9 @@ const App: FC = () => {
         </View>
         <View style={styles.wordContainer}>
           <Text style={styles.word} numberOfLines={1} ellipsizeMode='head'>{word}</Text>
+          {!!word &&
+            <Text style={[styles.validText, { color: validWord ? colors.GREEN : colors.RED }]} >{validWord ? 'valid' : 'invalid'}</Text>
+          }
         </View>
       </View>
     </View>
@@ -69,13 +80,13 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   clearText: {
-    color: 'grey',
+    color: colors.GREY,
     fontSize: 17
   },
   clearIcon: {
     width: 20,
     height: 20,
-    tintColor: 'grey'
+    tintColor: colors.GREY
   },
   wordContainer: {
     marginTop: 30,
@@ -84,13 +95,19 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
     paddingHorizontal: 15,
     paddingVertical: 7,
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   word: {
-    color: '#7ED321',
+    color: colors.GREEN,
     fontSize: 25,
     letterSpacing: 3,
     fontWeight: 'bold'
+  },
+  validText: {
+    opacity: 0.8,
+    textAlign: 'right'
   }
 });
 
---
+++ src/resources/colors.js
@@ -0,0 +1,5 @@
+const GREY = 'grey';
+const GREEN = '#7ED321';
+const RED = '#FF4A4A';
+
+export default { GREY, GREEN, RED };
